05782465808f1e1f4476b1b782c9d51fd380fd0d
u2berggeist <jrwrigh.iii@gmail.com>
2018-04-29 12:12:14 -0400

    fleshed out create_drivers function. Not debugged yet

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import pickle
 import pandas as pd
-import data
+import f1classes
+import re
 
-def create_drivers():
+
+def create_drivers(driverdict=None):
+    if not driverdict:
+        driverdict = {}
+
     raw = pd.read_clipboard()
-    
+    for data in raw.iterrows():
+        splitname = data['Name'].split(' ')
+        if len(splitname) <= 2:
+            name3 = splitname[1][0:4]
+
+        driverobj = f1classes.driver(data['Name'], data['Team'], name3)
+        driverdict[name3] = driverobj
+
 
 def data_creation():
-    raw = pd.read_clipboard()
+    # raw = pd.read_clipboard()
+    pass
 
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     print("helloworld")
-    
