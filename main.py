6c97ea6eabc927989f320351542d886b4c7f67f3
u2berggeist <jrwrigh.iii@gmail.com>
2018-05-01 19:58:00 -0400

    driver table, pickling, loading, and creating drivers now working

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pickle
 import pandas as pd
 import f1classes
 import re
+from IPython.core.debugger import set_trace
 
 
 def create_drivers(driverdict=None):
@@ -9,22 +10,44 @@ def create_drivers(driverdict=None):
         driverdict = {}
 
     raw = pd.read_clipboard()
-    for index, data in raw.iterrows():
+    for _, data in raw.iterrows():
         splitname = data['Name'].split(' ')
         if len(splitname) <= 2:
             name3 = splitname[1][0:3]
             name3 = name3.upper()
-        print(index)
 
         driverobj = f1classes.driver(data['Name'], data['Team'], name3)
         driverdict[name3] = driverobj
-    return(driverdict)
+    return (driverdict)
 
 
-def data_creation():
-    # raw = pd.read_clipboard()
+def add_DriverTable(driverdict):
+
+    driverdb = pd.DataFrame(columns=['Name', 'Team', 'Name3'])
+    for key in driverdict:
+        if key != "DriverTable":
+            driver = pd.DataFrame(
+                [driverdict[key].name, driverdict[key].constructor, key],
+                index=['Name', 'Team', 'Name3'])
+            driverdb = driverdb.append(driver.transpose(), ignore_index=True)
+    driverdict["DriverTable"] = driverdb
+
+
+def create_constructors():
     pass
 
 
+def pickle_drivers(driverdict, file=r'data\drivers.pkl'):
+    pickle.dump(driverdict, open(file, "wb"))
+
+
+def load_drivers(file=r'data\drivers.pkl'):
+    global driverdict
+    driverdict = pickle.load(open(file, 'rb'))
+
+
 if __name__ == "__main__":
-    driverdict = create_drivers()
+    # Use the below to create a drivers pickle to store their information
+    # driverdict = create_drivers()
+    # pickle_drivers(driverdict)
+    pass
---
+++ test.py
@@ -0,0 +1,4 @@
+import main
+
+main.load_drivers()
+main.add_DriverTable(main.driverdict)
