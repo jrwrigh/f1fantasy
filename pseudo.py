4fc96ccd8b153b354a08844546a09dbdc1845b89
u2berggeist <jrwrigh.iii@gmail.com>
2018-05-02 10:00:43 -0400

    pseudo code and others added

---
+++ pseudo.py
@@ -0,0 +1,10 @@
+def create_constructors(driverdict):
+    constructordict
+    for drvier in drivertable:
+        if driver.team not in construcdict.keys():
+            drivers = []
+            for driver in drivertable.filter[driver.team].iterrows():
+                drivers.append(driver['Name'])
+            f1fantasy.constructor(driver.team, drivers)
+
+            
