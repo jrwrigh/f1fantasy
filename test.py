6c9d6306dacc90a58bd27dbf6f10855a2631d83c
u2berggeist <jrwrigh.iii@gmail.com>
2018-05-03 13:17:50 -0400

    Misc

--- test.py
+++ test.py
@@ -5,10 +5,14 @@ import main
 reload(f1classes)
 reload(main)
 
+global test, test2
 if sys.argv[0] == 'race':
     test = f1classes.RaceWeekend()
     test.init_from_ergast()
     test.parse_raceresults()
+    if sys.argv[1] == 'pickleboth':
+        main.pickle_RaceWeekend(test)
+        test2 = main.load_RaceWeekend("data/races/2018_4-Azerbaijan")
 elif sys.argv[0] == 'drivertable':
     pass
 elif sys.argv[0] == 'reset':
