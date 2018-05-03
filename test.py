ad6bc7d3dc61db769df46f802ff793a95e2a6746
u2berggeist <jrwrigh.iii@gmail.com>
2018-05-03 11:54:37 -0400

    Added support for CLI arguments

--- test.py
+++ test.py
@@ -1,7 +1,15 @@
+import sys
 from importlib import reload
 import f1classes
+import main
 reload(f1classes)
+reload(main)
 
-test = f1classes.RaceWeekend()
-test.init_from_ergast()
-test.parse_raceresults()
+if sys.argv[0] == 'race':
+    test = f1classes.RaceWeekend()
+    test.init_from_ergast()
+    test.parse_raceresults()
+elif sys.argv[0] == 'drivertable':
+    pass
+elif sys.argv[0] == 'reset':
+    pass
