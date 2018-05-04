6b96799e16ebce8c849ec27478a4b44afe00ff21
u2berggeist <jrwrigh.iii@gmail.com>
2018-05-03 22:57:10 -0400

    Quali results retriable and parseable. pickling working confirmed

--- f1classes.py
+++ f1classes.py
@@ -7,7 +7,6 @@ import pandas as pd
 from IPython.core.debugger import set_trace
 
 
-
 class driver(object):
 
     def __init__(self, name, constructor, name3):
@@ -85,33 +84,35 @@ class constructor(object):
 class RaceWeekend(object):
 
     def __init__(self):
-        # self.raw = None
+        self.rawr = None
+        self.rawq = None
         pass
 
     def init_from_ergast(self, season=None, roundn=None):
         url_base = r'https://ergast.com/api/f1'
-        url_suffix_latest = r'/current/last/results'
         if not season or roundn:
-            url = url_base + url_suffix_latest + '.json'
-        rawjson = requests.get(url).json()
-        self.raw = namedtupled.map(rawjson)
-        self.Raceraw = self.raw.MRData.RaceTable.Races[0]
+            urlr = url_base + '/current/last/results' + '.json'
+        rawjson = requests.get(urlr).json()
+        self.rawr = namedtupled.map(rawjson)
+        self.Raceraw = self.rawr.MRData.RaceTable.Races[0]
+
+        if not season or roundn:
+            urlq = url_base + '/' + self.Raceraw.season + '/' + self.Raceraw.round + '/qualifying.json'
+        rawjson_quali = requests.get(urlq).json()
+        self.rawq = namedtupled.map(rawjson_quali)
+        self.Qualiraw = self.rawq.MRData.RaceTable.Races[0]
 
         self.season = int(self.Raceraw.season)
         self.roundn = int(self.Raceraw.round)
         self.raceName = self.Raceraw.raceName
-        dateraw = re.match(r'(\d{4})-(\d{2})-(\d{2})',
-                           self.Raceraw.date)
-
+        dateraw = re.match(r'(\d{4})-(\d{2})-(\d{2})', self.Raceraw.date)
         self.date = datetime.date(
             int(dateraw.group(1)), int(dateraw.group(2)), int(dateraw.group(2)))
         # TODO add qualifying stuff
         # http://ergast.com/mrd/methods/qualifying/
 
     def parse_raceresults(self):
-        try:
-            self.raw != None
-        except:
+        if self.rawr == None:
             raise Exception('Raw data has not been imported yet')
         Results = self.Raceraw.Results
         ResultsTable = pd.DataFrame(columns=[
@@ -134,7 +135,36 @@ class RaceWeekend(object):
             driver['Constructor'] = result.Constructor.name
             driver['Grid'] = int(result.grid)
             driver['Status'] = result.status
-            driver['Change'] = driver['Position'] - driver['Grid']
+            driver['Change'] = driver['Grid'] - driver['Position']
 
             ResultsTable = ResultsTable.append(driver, ignore_index=True)
-        self.ResultsTable = ResultsTable
+        self.RaceResultsTable = ResultsTable
+
+    def parse_qualiresults(self):
+        if self.rawq == None:
+            raise Exception('Raw data has not been imported yet')
+        Results = self.Qualiraw.QualifyingResults
+        ResultsTable = pd.DataFrame(columns=['Position','Name','Name3','Constructor','Q1','Q2','Q3'])
+
+        for result in Results:
+            driver = {}
+            driver['Position'] = int(result.position)
+            driver[
+                'Name'] = result.Driver.givenName + ' ' + result.Driver.familyName
+            driver['Name3'] = result.Driver.code
+            driver['Constructor'] = result.Constructor.name
+
+            if result.Q1 == '':
+                driver.update({'Q1':False,'Q2':False,'Q3':False})
+            elif result.Q1 != '':
+                if hasattr(result, 'Q2'):
+                    if hasattr(result, 'Q3'):
+                        driver.update({'Q1':True,'Q2':True,'Q3':True})
+                    else:
+                        driver.update({'Q1':True,'Q2':True,'Q3':False})
+                else:
+                    driver.update({'Q1':True,'Q2':False,'Q3':False})
+
+
+            ResultsTable = ResultsTable.append(driver, ignore_index=True)
+        self.QualiResultsTable = ResultsTable
